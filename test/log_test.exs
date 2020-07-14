6eca18e7fe5a969bae3651ab2918141fc5907beb
DevotuOne <devotu.developer@gmail.com>
2020-07-14 17:26:15 +0200

    Log - Refactor - Remove unused

--- test/log_test.exs
+++ test/log_test.exs
@@ -15,7 +15,6 @@ defmodule LogTest do
     sent_event = Event.new([:read, :log], %{number: number_requested})
     [resulting_event] = Log.feed sent_event
     assert [:list, :log, sent_event.id] == resulting_event.tags
-    IO.inspect(resulting_event, label: "logtest - resulting event")
     assert number_requested == Enum.count(resulting_event.data.entries)
   end
 end
