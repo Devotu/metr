5951a5134025fa932d983cdb97abe83725740f1e
DevotuOne <devotu.developer@gmail.com>
2020-07-25 10:42:50 +0200

    Refactor - Single update

--- lib/modules/player.ex
+++ lib/modules/player.ex
@@ -9,38 +9,22 @@ defmodule Metr.Player do
   alias Metr.Player
 
   def feed(%Event{id: _event_id, tags: [:create, :player], data: %{name: name}}) do
-    player_id = Id.hrid(name)
+    id = Id.hrid(name)
     #Log event #TODO replay by module?
     #Create state
     #The initialization is the only state change outside of a process
-    player_state = %Player{id: player_id, name: name}
+    player_state = %Player{id: id, name: name}
     #Save state
-    Data.save_state(__ENV__.module, player_id, player_state)
+    Data.save_state(__ENV__.module, id, player_state)
     #Start genserver
-    GenServer.start(Metr.Player, player_state, [name: Data.genserver_id(__ENV__.module, player_id)])
+    GenServer.start(Metr.Player, player_state, [name: Data.genserver_id(__ENV__.module, id)])
 
     #Return
-    [Event.new([:player, :created], %{id: player_id})]
+    [Event.new([:player, :created], %{id: id})]
   end
 
-  def feed(%Event{id: _event_id, tags: [:deck, :created], data: %{id: _deck_id, player_id: player_id}} = event) do
-    #Is running?
-    case GenServer.whereis(Data.genserver_id(__ENV__.module, player_id)) do
-      nil ->
-        #Get state
-        current_state = Data.recall_state(__ENV__.module, player_id)
-        #Start process
-        GenServer.start(Metr.Player, current_state, [name: Data.genserver_id(__ENV__.module, player_id)])
-        #Call update
-        msg = GenServer.call(Data.genserver_id(__ENV__.module, player_id), event)
-        #Return
-        [Event.new([:player, :altered], %{msg: msg})]
-      _ ->
-        #Call update
-        msg = GenServer.call(Data.genserver_id(__ENV__.module, player_id), event)
-        #Return
-        [Event.new([:player, :altered], %{msg: msg})]
-    end
+  def feed(%Event{id: _event_id, tags: [:deck, :created] = tags, data: %{id: _deck_id, player_id: id} = data}) do
+    update(id, tags, data)
   end
 
   def feed(%Event{id: _event_id, tags: [:game, :created] = tags, data: %{id: game_id, player_ids: player_ids}}) do
@@ -83,20 +67,20 @@ defmodule Metr.Player do
 
 
   @impl true
-  def handle_call(%{tags: [:deck, :created], data: %{id: deck_id, player_id: player_id}}, _from, state) do
+  def handle_call(%{tags: [:deck, :created], data: %{id: deck_id, player_id: id}}, _from, state) do
     new_state = Map.update!(state, :decks, &(&1 ++ [deck_id]))
     #Save state
-    Data.save_state(__ENV__.module, player_id, new_state)
+    Data.save_state(__ENV__.module, id, new_state)
     #Reply
-    {:reply, "Deck #{deck_id} added to player #{player_id}", new_state}
+    {:reply, "Deck #{deck_id} added to player #{id}", new_state}
   end
 
   @impl true
-  def handle_call(%{tags: [:game, :created], data: %{id: game_id, player_id: player_id}}, _from, state) do
+  def handle_call(%{tags: [:game, :created], data: %{id: game_id, player_id: id}}, _from, state) do
     new_state = Map.update!(state, :games, &(&1 ++ [game_id]))
     #Save state
-    Data.save_state(__ENV__.module, player_id, new_state)
+    Data.save_state(__ENV__.module, id, new_state)
     #Reply
-    {:reply, "Game #{game_id} added to player #{player_id}", new_state}
+    {:reply, "Game #{game_id} added to player #{id}", new_state}
   end
 end
