08ea82cb3913bc5ebd0c3a4278c52b033d460f57
DevotuOne <devotu.developer@gmail.com>
2020-12-09 23:10:31 +0100

    Base test - Refactor - Util function

--- test/base_test.exs
+++ test/base_test.exs
@@ -115,44 +115,20 @@ defmodule BaseTest do
   test "recall as correct struct" do
     player_name = "Erik Base"
     deck_name = "Echo Base"
+    {player_id, deck_id, match_id, game_id} = init_simple_states(player_name, deck_name)
 
-    [player_return] = Player.feed(Event.new([:create, :player], %{name: player_name}), nil)
-    player_id = player_return.data.id
     assert :ok == Data.genserver_id("Player", player_id) |> GenServer.stop()
     player = Player.read(player_id)
     assert is_struct(player, Player)
 
-    [deck_return] = Deck.feed(Event.new([:create, :deck], %{name: deck_name, player_id: player_id}), nil)
-    deck_id = deck_return.data.id
     assert :ok == Data.genserver_id("Deck", deck_id) |> GenServer.stop()
     deck = Deck.read(deck_id)
     assert is_struct(deck, Deck)
 
-    [match_return] =
-      Match.feed(
-        Event.new([:create, :match], %{
-          player_1_id: player_id,
-          deck_1_id: deck_id,
-          player_2_id: player_id,
-          deck_2_id: deck_id,
-          ranking: false
-        }),
-        nil
-      )
-    match_id = match_return.data.id
     assert :ok == Data.genserver_id("Match", match_id) |> GenServer.stop()
     match = Match.read(match_id)
     assert is_struct(match, Match)
 
-    game_input = %{
-      deck_1: deck_id,
-      deck_2: deck_id,
-      player_1: player_id,
-      player_2: player_id,
-      winner: 2,
-      match: match_id
-    }
-    game_id = Metr.create_game(game_input)
     assert :ok == Data.genserver_id("Game", game_id) |> GenServer.stop()
     game = Game.read(game_id)
     assert is_struct(game, Game)
@@ -177,5 +153,40 @@ defmodule BaseTest do
       results: [],
       time: 1607375096
     }
+  end
+
+
+  defp init_simple_states(player_name, deck_name) do
+
+    [player_return] = Player.feed(Event.new([:create, :player], %{name: player_name}), nil)
+    player_id = player_return.data.id
+
+    [deck_return] = Deck.feed(Event.new([:create, :deck], %{name: deck_name, player_id: player_id}), nil)
+    deck_id = deck_return.data.id
+
+    [match_return] =
+      Match.feed(
+        Event.new([:create, :match], %{
+          player_1_id: player_id,
+          deck_1_id: deck_id,
+          player_2_id: player_id,
+          deck_2_id: deck_id,
+          ranking: false
+        }),
+        nil
+      )
+    match_id = match_return.data.id
+
+    game_input = %{
+      deck_1: deck_id,
+      deck_2: deck_id,
+      player_1: player_id,
+      player_2: player_id,
+      winner: 2,
+      match: match_id
+    }
+    game_id = Metr.create_game(game_input)
+
+    {player_id, deck_id, match_id, game_id}
   end
 end
