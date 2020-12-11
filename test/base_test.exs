df038b2e4a02b6aa3a279e894c937224c4fb9071
DevotuOne <devotu.developer@gmail.com>
2020-12-11 23:13:31 +0100

    Refactor - Test helping function moved

--- test/base_test.exs
+++ test/base_test.exs
@@ -115,7 +115,7 @@ defmodule BaseTest do
   test "recall as correct struct" do
     player_name = "Erik Base"
     deck_name = "Echo Base"
-    {player_id, deck_id, match_id, game_id} = init_simple_states(player_name, deck_name)
+    {player_id, deck_id, match_id, game_id} = TestHelper.init_single_states(player_name, deck_name)
 
     assert :ok == Data.genserver_id("Player", player_id) |> GenServer.stop()
     player = Player.read(player_id)
@@ -147,7 +147,7 @@ defmodule BaseTest do
   test "rerun" do
     player_name = "Fredrik Base"
     deck_name = "Foxtrot Base"
-    {player_id, deck_id, match_id, game_id} = init_simple_states(player_name, deck_name)
+    {player_id, deck_id, match_id, game_id} = TestHelper.init_single_states(player_name, deck_name)
 
     original_player = Base.read(player_id, "Player")
     Data.wipe_state("Player", [player_id]) #To verify it is not the same state read
@@ -171,40 +171,5 @@ defmodule BaseTest do
     player_id = "fail_rerun_base"
     Base.rerun(player_id, "Player")
     assert {:error, "Player #{player_id} not found"} == Base.rerun(player_id, "Player")
-  end
-
-
-  defp init_simple_states(player_name, deck_name) do
-
-    [player_return] = Player.feed(Event.new([:create, :player], %{name: player_name}), nil)
-    player_id = player_return.data.id
-
-    [deck_return] = Deck.feed(Event.new([:create, :deck], %{name: deck_name, player_id: player_id}), nil)
-    deck_id = deck_return.data.id
-
-    [match_return] =
-      Match.feed(
-        Event.new([:create, :match], %{
-          player_1_id: player_id,
-          deck_1_id: deck_id,
-          player_2_id: player_id,
-          deck_2_id: deck_id,
-          ranking: false
-        }),
-        nil
-      )
-    match_id = match_return.data.id
-
-    game_input = %{
-      deck_1: deck_id,
-      deck_2: deck_id,
-      player_1: player_id,
-      player_2: player_id,
-      winner: 2,
-      match: match_id
-    }
-    game_id = Metr.create_game(game_input)
-
-    {player_id, deck_id, match_id, game_id}
   end
 end
