f022b3efabe7056298e6092ee395923e82316cfd
Ottu <otuvesson@gmail.com>
2021-05-15 22:50:43 +0200

    Only atom - Data test pass

--- test/data_test.exs
+++ test/data_test.exs
@@ -5,27 +5,27 @@ defmodule DataTest do
   alias Metr.Event
 
   test "log and retrieve by id" do
-    module_name = "DataTest_Log_and_retrieve_by_id"
+    module = :data_test_log_and_retrieve
     id1 = "id1"
     id2 = "id2"
 
-    Data.log_by_id(module_name, id1, Event.new([:correct], %{data: "you want this"}))
-    Data.log_by_id(module_name, id2, Event.new([:error], %{data: "this is not right"}))
-    Data.log_by_id(module_name, id1, Event.new([:correct], %{data: "you want this"}))
-    Data.log_by_id(module_name, id1, Event.new([:correct], %{data: "you want this"}))
+    Data.log_by_id(module, id1, Event.new([:correct], %{data: "you want this"}))
+    Data.log_by_id(module, id2, Event.new([:error], %{data: "this is not right"}))
+    Data.log_by_id(module, id1, Event.new([:correct], %{data: "you want this"}))
+    Data.log_by_id(module, id1, Event.new([:correct], %{data: "you want this"}))
 
-    entries = Data.read_log_by_id(id1, module_name)
+    entries = Data.read_log_by_id(id1, module)
     assert 3 == Enum.count(entries)
 
-    Data.wipe_log(module_name, id1)
-    Data.wipe_log(module_name, id2)
+    Data.wipe_log(module, id1)
+    Data.wipe_log(module, id2)
 
-    {:error, :not_found} = Data.read_log_by_id(id1, module_name)
-    {:error, :not_found} = Data.read_log_by_id(id2, module_name)
+    {:error, :not_found} = Data.read_log_by_id(id1, module)
+    {:error, :not_found} = Data.read_log_by_id(id2, module)
   end
 
   test "delimiter mixup" do
-    module_name = "DataTest_Delimiter_mixup"
+    module = :data_test_delimiter
     id = "id"
 
     # Becomes <<131, 98, 95, 192, 31, 42>> when turned into binary
@@ -33,16 +33,16 @@ defmodule DataTest do
     specific_time = 1_606_426_410
     specific_event = %Event{id: "dataTest_delimiter_mixup", time: specific_time}
 
-    Data.log_by_id(module_name, id, Event.new([:correct], %{data: "first"}))
-    Data.log_by_id(module_name, id, specific_event)
-    Data.log_by_id(module_name, id, Event.new([:correct], %{data: "second"}))
-    Data.log_by_id(module_name, id, Event.new([:correct], %{data: "third"}))
+    Data.log_by_id(module, id, Event.new([:correct], %{data: "first"}))
+    Data.log_by_id(module, id, specific_event)
+    Data.log_by_id(module, id, Event.new([:correct], %{data: "second"}))
+    Data.log_by_id(module, id, Event.new([:correct], %{data: "third"}))
 
-    entries = Data.read_log_by_id(id, module_name)
+    entries = Data.read_log_by_id(id, module)
     assert 4 == Enum.count(entries)
 
-    Data.wipe_log(module_name, id)
+    Data.wipe_log(module, id)
 
-    {:error, :not_found} = Data.read_log_by_id(id, module_name)
+    {:error, :not_found} = Data.read_log_by_id(id, module)
   end
 end
