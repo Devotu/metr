40a94f34f54c402548c9563dabf74d25d2dcc4ae
Ottu <otuvesson@gmail.com>
2021-06-06 22:49:09 +0200

    Restructure - Test read timeout

---
+++ test/state_test.exs
@@ -0,0 +1,47 @@
+defmodule StateTest do
+  use ExUnit.Case
+
+  alias Metr.Event
+  alias Metr.Modules.Input.PlayerInput
+  alias Metr.Modules.State
+
+  test "standoff read entity x" do
+    player_id = "adam_state_fake_id"
+    player_input = %PlayerInput{name: "Adam State"}
+    player_create_event = Event.new(
+      [:create, :player],
+      %{
+        id: player_id,
+        input: player_input
+      }
+    )
+
+    read_task = Task.async(fn -> Metr.read(player_id, :player) end)
+    :timer.sleep(50)
+    State.feed(player_create_event, nil)
+
+    assert {:error, "player adam_state_fake_id not found"} != Task.await(read_task)
+
+    TestHelper.wipe_test(:player, player_id)
+  end
+
+  test "standoff fail read entity x" do
+    player_id = "bertil_state_fake_id"
+    player_input = %PlayerInput{name: "Bertil State"}
+    player_create_event = Event.new(
+      [:create, :player],
+      %{
+        id: player_id,
+        input: player_input
+      }
+    )
+
+    read_task = Task.async(fn -> Metr.read(player_id, :player) end)
+    :timer.sleep(200)
+    State.feed(player_create_event, nil)
+    
+    assert {:error, "player bertil_state_fake_id not found"} == Task.await(read_task)
+
+    TestHelper.wipe_test(:player, player_id)
+  end
+end
